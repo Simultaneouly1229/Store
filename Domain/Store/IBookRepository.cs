6e6ab7c1f26d968f0ab27b8f0ca6bd2707c96f24
Kirill <kirill123novitskiy@gmail.com>
2022-10-16 19:26:01 +0300

    Add search-page

--- Domain/Store/IBookRepository.cs
+++ Domain/Store/IBookRepository.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Store
 {
-    internal interface IBookRepository
+    public interface IBookRepository
     {
+        Book[] GetBooksByTitle(string titlePart);
+        
     }
 }
--- Infrastructure/Store.Data/BookRepository.cs
+++ Infrastructure/Store.Data/BookRepository.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Linq;
 using Store;
 
 namespace Store.Data
 {
     public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art Of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
 
+        public Book[] GetBooksByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
     }
 }
